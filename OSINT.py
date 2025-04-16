ae69cf9216ce1304573546e9b1bc9dbcc3ea224c
wy-tt <wyatt.fleisher@live.com>
2025-04-16 12:56:17 -0400

    Updates: Turned Argparse Section into "main()" function called at bottom of OSINT.py. Handle Zero Arguments Supplied with check of len(sys.argv) to print reccommended "-h" check. handle looping throuh multiple arguments and executing all functions currently built and future functions yet created. Added Beginnings of File Reputation Searches Including taking user input about what Hash Algorithm type was supplied, Next step is to review and Parse out important fields about the file rep JSON return.

--- OSINT.py
+++ OSINT.py
@@ -3,6 +3,40 @@ import json
 import argparse
 import requests
 import re
+import os
+import sys
+
+def main():
+  # ArgParse Options
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-d", help="Domain address to Search", action='store', nargs='*')
+  parser.add_argument("-e", help="Email address to Search Reputation", action='store', nargs='*')
+  parser.add_argument("-f", "--file", help="File Hash to Check", action='store', nargs='*')
+  parser.add_argument("-i", "--ip", help="IP Address for Search", action='store', nargs='*')
+  parser.add_argument("-u", help="URL For Search and Scan", action='store',)
+  parser.add_argument("-r", help='Raw Data output to Individual files', action='store_true')
+  parser.add_argument("-v", help='Add Verbose return Information', action='store_true')
+  
+  args = parser.parse_args()
+
+  if len(sys.argv) == 1:
+    print("No Arguments Provided try running with '-h' to get help information")
+
+  # Loop Through multiple Arguments and execute Functions Associated
+  args2 = vars(args)
+  for var, input in args2.items():
+    if (input != None) & (var == 'ip'):
+      print(f"IP Address(s) Provided: {args.ip}")
+      iphistloop(args)
+    elif (input != None) & (var == 'file'):
+      print(f"File Hash Provided: {args.file}")
+      hashloop(args)
+    elif (input != None) & (var == 'd'):
+      print(f"Domain Present: {input}")
+    elif (input != None) & (var == 'e'):
+      print(f"Email Present: {input}")
+    elif (input != None) & (var == 'u'):
+      print(f"URL Present: {input}")
 
 # API Key retrieve
 with open('Config.json') as user_file:
@@ -14,18 +48,6 @@ api_keys = json.loads(api_contents)
 Virustotal = api_keys["Virus Total"]
 AbuseIPDB = api_keys["AbuseIPDB"]
 GreyNoise = api_keys["GreyNoise"]
-
-# ArgParse Options
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", help="Domain address to Search", action='store', default='None')
-parser.add_argument("-f", help="File Hash Check", action='store', default='None')
-parser.add_argument("-i", "--ip", help="IP Address for Search", action='store', default='None', nargs='*')
-parser.add_argument("-u", help="URL For Search and Scan", action='store', default='None')
-parser.add_argument('-r', help='Raw Data output', action='store_true', default='None')
-parser.add_argument('-v', help='Add Verbose return Information', action='store_true', default='None')
-args = parser.parse_args()
-
-print(args)
 
 def verbose_outfile(arg, vtiphist, ipdbhist, greyhist):
     print("Building Verbose IP History File")
@@ -42,7 +64,7 @@ def verbose_outfile(arg, vtiphist, ipdbhist, greyhist):
     whitelist = ipdbhist["data"]["isWhitelisted"]
     domain = ipdbhist["data"]["domain"]
     classification = greyhist["classification"]
-    outfile = open(f"{arg}.txt", 'a')
+    outfile = open(f"{arg}.txt", 'w')
     outfile.write(f"""Report from Multiple sources:
 -------------HEADLINE INFORMATION-------------------
 Total Votes from Virus Total: {votes}
@@ -78,7 +100,7 @@ def write_outfile(arg, vtiphist, ipdbhist, greyhist):
     confidence = ipdbhist["data"]["abuseConfidenceScore"]
     country2 = ipdbhist["data"]["countryCode"]
     classification = greyhist["classification"]
-    outfile = open(f"{arg}.txt", 'a')
+    outfile = open(f"{arg}.txt", 'w')
     outfile.write(f"""Report from Multiple sources:
 -------------HEADLINE INFORMATION-------------------
 Total Votes from Virus Total: {votes}
@@ -98,7 +120,22 @@ Certificate Valid Dates:
 {valid}
 """)
 
-def IP_Hist(arg):
+def print_raw(arg, vtiphist, ipdbhist):
+  home_dir = os.path.expanduser("~")
+  raw_vt = f"raw-{arg}-VirusTotal.json"
+  file_vt = os.path.join(home_dir, raw_vt)
+  raw_ipdb = f"raw-{arg}-AbuseIPDB.json"
+  file_ipdb = os.path.join(home_dir, raw_ipdb)
+  with open(file_vt, 'w') as file:
+    print(vtiphist)
+    file.close
+    print(f"Virus Total IP Information Json File stored at {file_vt}")
+  with open(file_ipdb, 'w') as file:
+    print(ipdbhist)
+    file.close
+    print(f"AbuseIPDB IP Information Json File stored at {file_ipdb}")
+
+def IP_Hist(arg, raw, verbose):
   #Virus Total API Request
   vt_url = f'https://www.virustotal.com/api/v3/ip_addresses/{arg}'
   vt_headers = {
@@ -135,18 +172,56 @@ def IP_Hist(arg):
   greyhist = json.loads(grey_response.text)
 
   #Call Output File Function
-  if args.v == True:
+  if raw == True:
+    print("Printing Raw Outputs to files")
+    print_raw(arg, vtiphist, ipdbhist)
+  elif verbose == True:
     print("Print Verbose Output File")
     verbose_outfile(arg, vtiphist, ipdbhist, greyhist)
   else:
     write_outfile(arg, vtiphist, ipdbhist, greyhist)
 
-# Confirm IP format and Build function trggers from Arguments
-ipv4_pattern = r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}"
-#This doesn't completely filter out values over 255.255.255.255 but catches letters and blank/missing fields
-for arg in args.ip:
-  print(arg)
-  if re.match(ipv4_pattern, arg, re.ASCII):
-    IP_Hist(arg)
-  else:
-     print("Recheck IP address for formatting and values below 255.255.255.255")
+def filerep(arg, hashsearch):
+  url = f'https://www.virustotal.com/api/v3/files/{arg}'
+  headers = {
+    'accept': 'application/json',
+    'x-apikey': f'{Virustotal}'
+    }
+  response = requests.get(url, headers=headers)
+  print(response.text)
+
+# Confirm IP format and Start IP History Loop
+def iphistloop(args):
+  print('Loop executed')
+  ipv4_pattern = r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}"
+  raw = args.r
+  verbose = args.v
+  #This doesn't completely filter out values over 255.255.255.255 but catches letters and blank/missing fields
+  for arg in args.ip:
+    if re.match(ipv4_pattern, arg, re.ASCII):
+      IP_Hist(arg, raw, verbose)
+    else:
+      print("Recheck IP address for formatting and values below 255.255.255.255")
+
+# Grab Hash Argument and run Hash Check loop
+def hashloop(args):
+  hashtype = input("""Please Choose the Hash Algorithm for Search:
+1. MD5
+2. SHA256
+3. SHA 512
+""")
+  for val in args.file:
+    if hashtype == '1':
+      hashsearch = 'md5'
+      filerep(val, hashsearch)
+    elif hashtype == '2':
+      hashsearch = 'sha256'
+      filerep(val, hashsearch)
+    elif hashtype == '3':
+      hashsearch = 'sha512'
+      filerep(val, hashsearch)
+    else:
+      print("The only currently supported values are 1, 2, 3. Please re-run the program.")
+
+if __name__ == "__main__":
+  main()
